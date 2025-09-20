4a1e19042154611d4b3164fa5c254f105a593149
Hassan Fahmy <fahmy.hassan@gmail.com>
2025-09-20 16:19:52 +0500

    Initial commit

---
+++ gradient-descent.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+
+points = np.array(
+    [0, 1.4],
+    [2.3, 1.9],
+    [2.9, 3.2]
+)
+
+# Gradient descent steps
+# 1. Initialize parameters
+initial_guess_a, initial_guess_b = 0, 0
+learning_rate = 0.01
+max_iterations = 1000
+
+
+
