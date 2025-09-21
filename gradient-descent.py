3aec44c63fc5900dce2801a9d3e44a3e42c13c31
Hassan Fahmy <fahmy.hassan@gmail.com>
2025-09-21 16:52:14 +0500

    fix: syntax

--- gradient-descent.py
+++ gradient-descent.py
@@ -3,15 +3,15 @@ import pandas as pd
 import matplotlib as plt
 
 
-points = np.array(
+points = np.array([
     [0, 1.4],
     [2.3, 1.9],
     [2.9, 3.2]
-)
+])
 
 # Gradient descent steps
 # 1. Initialize parameters
-initial_guess_a, initial_guess_b = 0, 0
+initial_guess_a, initial_guess_b = 0, 1
 learning_rate = 0.01
 max_iterations = 1000
 
